07ed476deaaac31cd3eb41423fc9be578793bea9
Yashraj Pawar <yashrajpawar9604@gmail.com>
2024-10-05 23:09:52 +0530

    about functions in deep

--- 03_basics/Functions.js
+++ 03_basics/Functions.js
@@ -37,4 +37,42 @@ function loginUserMessage(username = "yash"){
   console.log( loginUserMessage("yash"));
   
   console.log( loginUserMessage());
-  
+
+
+  function calculateCartPrice(num1){
+    return num1
+  }
+  function calculateCartPrice(val1, val2,...num1){
+    return num1
+  }
+
+console.log(calculateCartPrice(200, 300, 500, 2000));
+
+
+
+
+const user = {
+  username: "yash",
+  price: 139
+}
+function handleObject(anyobject){
+console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+
+// handleObject(user);
+handleObject({
+  username: "samay",
+  price: 599
+}
+
+)
+const myNewArray = [200, 400, 500, 700]
+
+function returnSecondValue(getarray){
+  return getarray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 700]));
+
