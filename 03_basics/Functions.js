44a3bedd6a3bb494c64eb1341174e530a380176b
Yashraj Pawar <yashrajpawar9604@gmail.com>
2024-10-05 23:13:06 +0530

    changes in Code

--- 03_basics/Functions.js
+++ 03_basics/Functions.js
@@ -73,6 +73,6 @@ function returnSecondValue(getarray){
   return getarray[1]
 }
 
-// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 500, 700]));
 
